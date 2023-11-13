cb1e3c9ce185c85aa1cf368237758b70063f7ebc
tsenturion <80580892+tsenturion@users.noreply.github.com>
2023-11-13 23:04:58 +0500

    7/53 fix

--- NodejsConsoleApp1/myfunction.ts
+++ NodejsConsoleApp1/myfunction.ts
@@ -1,4 +1,4 @@
-﻿const isComplete = (course: { name: string, lessons: string[], }) => course.lessons.length > 3;
+﻿const isComplete = (course: { name: string, lessons: string[], }): boolean => course.lessons.length > 3;
 
 
 export default isComplete;
